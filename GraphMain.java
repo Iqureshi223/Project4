340ff3ad79663a4208d0d07a6d4d03cd75743244
Imaad <Iqureshi@mail.umw.edu>
2022-04-03 19:01:14 +0000

    read in file and created graph object

--- GraphMain.java
+++ GraphMain.java
@@ -1,10 +1,19 @@
-import java.io.*
-import java.util*
-public class Main{
+import java.io.*;
+import java.util.*;
+public class GraphMain{
 	public static void main (String args[]) { 
-		
+	String fileName = args[0];
+		try{
+			FileInputStream file = new FileInputStream(fileName);
+			Scanner in = new Scanner(file);
+			System.out.println("reading in file....");
+			int size = in.nextInt();
+			Graph graph = new Graph(size);
+			
 
-
+		}catch (FileNotFoundException e) {
+			System.out.println("File" + fileName + " could not be opened");
+			System.exit(1);
 
 
 
@@ -12,4 +21,5 @@ public class Main{
 
 
 
+	}
 }
